eb5735063cedf3afc971123a621285e56965abfe
Micaela Barles <micaelabarles@gmail.com>
2022-09-29 18:03:08 +0100

    tercer desafio

---
+++ desafio 3/contenedor.js
@@ -0,0 +1,75 @@
+const fs = require('fs')
+const express = require('express')
+
+const app = express()
+
+app.listen(8080, () => {
+    console.log("listo")
+})
+
+class Contenedor{
+    constructor(file){
+        this.file = file
+
+    }
+
+save = async(product) =>{
+    try {
+        if(fs.existsSync(this.file)){
+            const contenido = await fs.promises.readFile(this.file, "utf-8")
+            if(contenido){
+                const contenidoProductos = JSON.parse(contenido)
+                const nuevoProducto = {
+                    id:contenidoProductos.length+1,
+                    ...product
+                }
+                contenidoProductos.push(nuevoProducto)
+                await fs.promises.writeFile(this.file, JSON.stringify(contenidoProductos, null, 2))
+                
+                app.get("/productos",(req, res)=>{
+                    res.send(contenidoProductos)
+                    })
+
+               const productoRandom = contenidoProductos[Math.floor(Math.random()*contenidoProductos.length)]        
+
+               app.get("/productoRandom", (req,res)=>{
+                res.send(productoRandom)
+               })     
+            } else{
+                const nuevoProducto = {
+                    id:1,
+                    ...product
+                }
+                await fs.promises.writeFile(this.file, JSON.stringify([nuevoProducto], null, 2))
+            }
+        } 
+        else {
+            const nuevoProducto = {
+                id:1,
+                ...product
+            }
+           await fs.promises.writeFile(this.file, JSON.stringify([nuevoProducto], null, 2))
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+}
+
+const productos = new Contenedor("productos.txt")
+
+const producto1 = {title:"product1",price:123}
+const producto2 = {title:"product2",price:234}
+const producto3 = {title:"product3",price:567}
+
+const crearProductos = async()=>{
+    await productos.save(producto1)
+    await productos.save(producto2)
+    await productos.save(producto3)
+
+}
+
+crearProductos()
+
+
